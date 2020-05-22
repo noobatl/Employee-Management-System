cb22c9f0f10af0bc799ada5ace896f60b333d632
noobatl <info@noobusa.com>
2020-05-21 22:53:26 -0400

    Edited typo in Schema

--- emsSchema.sql
+++ emsSchema.sql
@@ -32,7 +32,7 @@ CREATE TABLE role(
 );
 
 -- * **employee**:
-CREATE TABLE emmployee(
+CREATE TABLE employee(
   --   * **id** - INT PRIMARY KEY
   id INT NOT NULL AUTO_INCREMENT,
   --   * **first_name** - VARCHAR(30) to hold employee first name
